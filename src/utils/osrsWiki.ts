c4829c01a240ce6a3574e430545948e4ee920e9c
Armando Vasquez <mando@mcnuggies.dev>
2025-08-23 16:43:54 -0500

    Use wikijs (#7)
    * use wikijs for search tool; implement mirrored types for wikijs
    
    * use wikijs implementation for summary tool
    
    * use wikijs implementation to get page rawContent

---
+++ src/utils/osrsWiki.ts
@@ -0,0 +1,11 @@
+import wiki from 'wikijs';
+import { osrsApiBaseUrl } from './constants';
+import type { WikiInstance } from '../types/wikijs';
+
+const osrsWiki = wiki({
+	apiUrl: osrsApiBaseUrl,
+	origin: null,
+}) as WikiInstance
+
+export const searchWikiForTopicMatches = (topic: string, limit: number) => osrsWiki.search(topic, limit)
+export const getPageForTopic = (topic: string) => osrsWiki.page(topic)
