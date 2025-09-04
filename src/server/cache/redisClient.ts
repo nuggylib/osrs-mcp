00a69f970bf9cc6767a0cb2832207d8e27eb1646
Armando Vasquez <mando@mcnuggies.dev>
2025-09-04 01:04:19 -0500

    use REDISCLOUD_URL (set by Heroku Redis Cloud app)

--- src/server/cache/redisClient.ts
+++ src/server/cache/redisClient.ts
@@ -1,6 +1,7 @@
 import Redis from 'ioredis';
 
-const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+// Either use the REDISCLOUD_URL variable (set by Redis Cloud addon), or the local address
+const REDIS_URL = process.env.REDISCLOUD_URL || 'redis://localhost:6379';
 
 export const redis = new Redis(REDIS_URL, {
 	retryStrategy: (times: number) => {
